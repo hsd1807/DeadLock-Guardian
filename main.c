99c33a6d231703641396bc3a1c4d7a5ae9f38062
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-08-30 21:05:32 +0530

    Updated main.c

--- main.c
+++ main.c
@@ -32,23 +32,43 @@ void cleanup_system(SystemState *state);
 
 #include "initialization/initialize_system.h"
 #include "initialization/user_defined_input.h"
+#include "initialization/print_file_format_instructions.h"
+#include "initialization/input_from_file.h"
+
 
 int main() {
 	SystemState state;
+	clock_t start_time, end_time;
 
 	welcome_user();
 
 	while (true) {
 		if (initialize_system(&state)) {
+			start_time = clock();
+			validate_and_sanitize_input(&state);
+
 			print_system_state(&state);
+
+			printf("\nSystem State after getting validated and sanitized\n\n");
+
 			
 			bool exit = false;
-
+			
 			if (!simulation_mode(&state)) {
 				exit = true;
 			}
+			end_time = clock();
+
+			generate_report(&state, start_time, end_time);
+
 			cleanup_system(&state);
+
 			if (exit) break;
+
+			printf("\nDo you want to run another simulation? (1/0):  ");
+			int choice;
+			scanf("%d", &choice);
+			if (!choice) break;
 		}
 		else {
 			break;
